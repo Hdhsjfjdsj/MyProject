2c4617cbc9b854ee8c2c089c5b2f1c9318940412
“hdhsjfjdsj” <eshaan.hazarika05@gmail.com>
2025-10-03 09:09:13 +0530

    added power and square root

--- src/calculator.py
+++ src/calculator.py
@@ -34,6 +34,22 @@ def divide(a, b):
 
 
 # TODO: Students will add power, sqrt functions
+def power(a, b):
+
+    """Raise a to the power of b"""
+
+    return a ** b
+
+def square_root(a):
+
+    """Calculate square root of a"""
+
+    if a < 0:
+
+        raise ValueError("Cannot calculate square root of negative number")
+    return a ** 0.5
+
+
 
 if __name__ == "__main__":
     print("🧮 Calculator Module")
